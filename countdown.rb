19f2e16dcfd15140a983b8d7559f2b449e63903a
Michael Ernest <m.hendersun@gmail.com>
2020-09-30 18:51:24 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,11 +1,10 @@
 #write your code here
 
 def countdown(n)
-  while n >= 0
+  while n > 0
     puts "#{n} SECOND(S)!"
     n -= 1
   end
-  puts 'HAPPY NEW YEAR!'
   return 'HAPPY NEW YEAR!'
 end
 
